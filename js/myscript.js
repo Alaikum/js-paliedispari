3b96a86da63e09b6bee79a02a2aad88eec7e79ad
Alaikum <carangiadamo@gmail.com>
2022-07-28 17:16:18 +0200

    Function pariOdispari

--- js/myscript.js
+++ js/myscript.js
@@ -6,15 +6,7 @@ console.log('Pali e Dispari')
 // Creare una funzione per capire se la parola inserita
 //  è palindroma
 
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero 
-// da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il 
-// computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari 
-// (usando una funzione)
-// Dichiariamo chi ha vinto.
+
 
 
 // faccio inserire la parola 
@@ -26,6 +18,9 @@ const parola = prompt('Inserisci una parola')
 isPalindrome(parola)
 
 function isPalindrome(word) {
+
+
+
     // creo un array composto dalle lettere  da cui poi 
     // ottengo una stringa
     let arrayWord = word.split('').join('')
@@ -49,3 +44,84 @@ function isPalindrome(word) {
     return
 
 }
+
+
+
+
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero 
+// da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il 
+// computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari 
+// (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// funzione per numeri random da 1 a 5 
+
+function randomNumberOneFive() {
+
+    let num = Math.floor(Math.random() * 5 + 1)
+    return num
+}
+
+
+// funzione per capire se un numero è pari,
+// se è falso da FALSE come risultato
+function isEven(number) {
+    let even = false
+    if (number % 2 === 0) {
+        even = true
+
+    }
+    return even
+}
+
+
+
+// creo il collegamento con il bottone 
+const buttonElement = document.getElementById('play')
+
+// metto l'evento che da inizio al gioco 
+buttonElement.addEventListener('click', function () {
+
+    const pariOdispari = document.getElementById('pari-o-dispari')
+    // console.log(pariOdispari.value)
+    const sceltaGiocatore = parseFloat(pariOdispari.value)
+
+    const numeroScelto = document.getElementById('numero-scelto')
+    // console.log(numeroScelto.value)
+
+    // lo trasformo in un valore numerico 
+    const numeroDelGiocatore = parseFloat(numeroScelto.value)
+    // console.log(numeroDelGiocatore)
+    const numeroComputer = randomNumberOneFive()
+
+    let somma = numeroDelGiocatore + numeroComputer
+    console.log(somma)
+
+    let risultato = isEven(somma)
+
+
+    // tre opzioni vittori con pariOdispari, vittoria con dispari
+    // o sconfitta 
+    if (sceltaGiocatore === 0 && risultato === true) {
+        console.log('hai vinto')
+
+    } else if (sceltaGiocatore === 1 && risultato === false) {
+        console.log('hai vinto')
+    } else {
+        console.log('hai perso')
+    }
+
+
+
+
+
+
+})
+
+
+
+
